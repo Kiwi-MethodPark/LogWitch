9191bfabab80e7f4ab2f1ea7611005ab2e0dd6cf
Sven Steckmann <spm_gitcommit@steckmann.de>
2011-05-14 13:09:43 +0200

    Adds header for assertion

---
+++ Assert.h
@@ -0,0 +1,15 @@
+/*
+ * Assert.h
+ *
+ *  Created on: May 14, 2011
+ *      Author: sven
+ */
+
+#ifndef ASSERT_H_
+#define ASSERT_H_
+
+#define LFA_ASSERT( cond, message ) do{ }while(0)
+#define LFA_ASSERT_D( cond, message ) do{ }while(0)
+
+
+#endif /* ASSERT_H_ */
