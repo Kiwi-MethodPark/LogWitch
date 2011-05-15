13e6012a28197a73acb4807b53a2981577e1538f
Sven Steckmann <spm_gitcommit@steckmann.de>
2011-05-15 16:56:32 +0200

    Add new Metatype TSharedConstQString

---
+++ Types.h
@@ -0,0 +1,20 @@
+/*
+ * Types.h
+ *
+ *  Created on: May 15, 2011
+ *      Author: sven
+ */
+
+#ifndef TYPES_H_
+#define TYPES_H_
+#include <boost/shared_ptr.hpp>
+
+#include <QMetaType>
+
+class QString;
+
+typedef boost::shared_ptr<const QString> TSharedConstQString;
+
+Q_DECLARE_METATYPE ( TSharedConstQString );
+
+#endif /* TYPES_H_ */
--- main.cpp
+++ main.cpp
@@ -6,12 +6,14 @@
 #include <QApplication>
 
 #include "LogData/LogEntry.h"
+#include "Types.h"
 
 
 int main(int argc, char *argv[])
 {
-	 int id = qRegisterMetaType<TSharedLogEntry>("TSharedLogEntry");
-	 std::cout << " TSharedLogEntry registered to id: " << id << std::endl;
+	 qRegisterMetaType<TSharedLogEntry>("TSharedLogEntry");
+	 qRegisterMetaType<TSharedConstQString>("TSharedConstQString");
+
 
     QApplication a(argc, argv);
     LogfileAnalyser w;
