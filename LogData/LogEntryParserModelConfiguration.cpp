b5fbdf64b9f7459d2409ccba04215ac57bb240be
Sven Steckmann <spm_gitcommit@steckmann.de>
2011-05-18 23:09:33 +0200

    Adds LogEntryParser Model

---
+++ LogData/LogEntryParserModelConfiguration.cpp
@@ -0,0 +1,17 @@
+/*
+ * LogEntryParserModelConfiguration.cpp
+ *
+ *  Created on: May 18, 2011
+ *      Author: sven
+ */
+
+#include "LogEntryParserModelConfiguration.h"
+
+LogEntryParserModelConfiguration::LogEntryParserModelConfiguration() {
+	// TODO Auto-generated constructor stub
+
+}
+
+LogEntryParserModelConfiguration::~LogEntryParserModelConfiguration() {
+	// TODO Auto-generated destructor stub
+}
---
+++ LogData/LogEntryParserModelConfiguration.h
@@ -0,0 +1,67 @@
+/*
+ * LogEntryParserModelConfiguration.h
+ *
+ *  Created on: May 18, 2011
+ *      Author: sven
+ */
+
+#ifndef LOGENTRYPARSERMODELCONFIGURATION_H_
+#define LOGENTRYPARSERMODELCONFIGURATION_H_
+#include <QtCore/QtCore>
+#include <boost/shared_ptr.hpp>
+
+
+class EntryToTextFormater;
+class LogEntryAttributeFactory;
+/**
+ * This class is responsible for modeling different kinds of behavior for the
+ * GUI which is defined by the logfile and its structure.
+ */
+class LogEntryParserModelConfiguration
+{
+public:
+	LogEntryParserModelConfiguration();
+	virtual ~LogEntryParserModelConfiguration();
+
+	/**
+	 * Returns if an entry field is a hierarchy and returns the string for a regex to split it.
+	 */
+	QString getHierarchySplitString( int idx ) const
+	{
+		if( idx >= 0 && idx < m_hierarchySplitstrings.size() )
+			return m_hierarchySplitstrings[idx];
+		else
+			return "";
+	}
+
+	void setHierarchySplitString( int idx, const QString & fmt)
+	{
+		if( idx >= m_hierarchySplitstrings.size() )
+			m_hierarchySplitstrings.resize(idx+1);
+
+		m_hierarchySplitstrings[idx] = fmt;
+	}
+
+	/**
+	 * Returns an entry to text formater for this types of entries.
+	 */
+	boost::shared_ptr<EntryToTextFormater> getEntryToTextFormater() const { return m_formater;};
+
+	void setEntryToTextFormater( boost::shared_ptr<EntryToTextFormater> fmt ){ m_formater = fmt;}
+
+	/**
+	 * Returns the corresponding factory for creating log entries.
+	 */
+	boost::shared_ptr<const LogEntryAttributeFactory> getLogEntryAttributeFactory() const { return m_attr;}
+
+	void setLogEntryAttributeFactory( boost::shared_ptr<LogEntryAttributeFactory> factory) { m_attr = factory;}
+
+private:
+	QVector<QString> m_hierarchySplitstrings;
+
+	boost::shared_ptr<EntryToTextFormater> m_formater;
+
+	boost::shared_ptr<LogEntryAttributeFactory> m_attr;
+};
+
+#endif /* LOGENTRYPARSERMODELCONFIGURATION_H_ */
