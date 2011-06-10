5142efabab1b0e62ef1176787f540fe927e8ba06
Sven Steckmann <spm_gitcommit@steckmann.de>
2011-06-10 23:52:26 +0200

    Implements Rule table

--- ActionRules/RuleTable.h
+++ ActionRules/RuleTable.h
@@ -10,6 +10,7 @@
 #include <list>
 
 #include <boost/shared_ptr.hpp>
+#include <QtCore/qobject.h>
 
 #include "Rule.h"
 
@@ -19,30 +20,52 @@ typedef boost::shared_ptr<RuleTable> TSharedRuleTable;
 typedef boost::shared_ptr<const RuleTable> TconstSharedRuleTable;
 
 class RuleTable
+    :public QObject
 {
+    Q_OBJECT
 public:
+    typedef std::list< TconstSharedRule > TRuleList;
+
     RuleTable();
     virtual ~RuleTable();
 
-    template <class T>
-    TSharedRuleTable filterRulesByType( ) const
-    {
-        TSharedRuleTable table( new RuleTable );
-
-        TRuleList::iterator it;
-        for( it = m_rules.begin(); it != m_rules.end(); ++it )
-        {
-            boost::shared_ptr<T> rule = boost::dynamic_pointer_cast<T>( *it );
-            if( rule )
-                table->m_rules.push_back( rule );
-        }
-        return table;
-    }
+//    template <class T>
+//    TSharedRuleTable filterRulesByType( ) const
+//    {
+//        TSharedRuleTable table( new RuleTable );
+//
+//        TRuleList::iterator it;
+//        for( it = m_rules.begin(); it != m_rules.end(); ++it )
+//        {
+//            boost::shared_ptr<T> rule = boost::dynamic_pointer_cast<T>( *it );
+//            if( rule )
+//                table->m_rules.push_back( rule );
+//        }
+//        return table;
+//    }
 
     void addRule( TSharedRule &rule );
 
+    // TActionList getActionsForEntry( TconstSharedLogEntry &entry ) const;
+    template <class T>
+    bool getMatchedActionsForType( std::list<T> &actions, TconstSharedLogEntry &entry ) const
+    {
+        TRuleList::const_iterator it;
+        for( it = m_rules.begin(); it != m_rules.end(); ++it )
+        {
+            T action = boost::dynamic_pointer_cast<typename T::element_type>( (*it)->getAction() );
+            if( action && (*it)->checkRule( entry ) )
+                actions.push_back( action );
+        }
+        return !actions.empty();
+    }
+
+signals:
+    void beginChange();
+    void endChange();
+
 private:
-    typedef std::list< TconstSharedRule > TRuleList;
+
     TRuleList m_rules;
 };
 
