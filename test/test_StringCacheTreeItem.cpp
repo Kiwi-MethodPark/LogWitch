218effefb5e24ee175f86d50bc7b5748190370ad
Sven Steckmann <spm_gitcommit@steckmann.de>
2012-05-01 21:59:46 +0200

    Adds test for CheckItems.

--- test/test_StringCacheTreeItem.cpp
+++ test/test_StringCacheTreeItem.cpp
@@ -39,5 +39,38 @@ BOOST_AUTO_TEST_CASE( addItems )
     BOOST_CHECK_EQUAL( root.childCount(), 2);
 }
 
+BOOST_AUTO_TEST_CASE( checkItems )
+{
+    StringCacheTreeItem root(make_shared<const QString>("Root")
+            , make_shared<const QString>("Root"));
 
+    StringCacheTreeItem *RootA = new StringCacheTreeItem(
+              make_shared<const QString>("A")
+            , make_shared<const QString>("Root.A") ) ;
+    root.appendChild( RootA );
+
+    StringCacheTreeItem *RootB = new StringCacheTreeItem(
+              make_shared<const QString>("B")
+            , make_shared<const QString>("Root.B") ) ;
+    root.appendChild( RootB );
+
+    StringCacheTreeItem *RootA1 = new StringCacheTreeItem(
+              make_shared<const QString>("1")
+            , make_shared<const QString>("Root.A.1") ) ;
+    RootA->appendChild( RootA1 );
+
+    // Structure building finished. Do consistency check
+    BOOST_CHECK_EQUAL( root.childCount(), 2 );
+    BOOST_CHECK_EQUAL( RootA->childCount(), 1 );
+    BOOST_CHECK_EQUAL( RootA1->childCount(), 0 );
+    BOOST_CHECK_EQUAL( RootB->childCount(), 0 );
+
+    BOOST_CHECK_EQUAL( root.getChecked(), Qt::Checked );
+    BOOST_CHECK_EQUAL( RootA->getChecked(), Qt::Checked );
+    BOOST_CHECK_EQUAL( RootA1->getChecked(), Qt::Checked );
+    BOOST_CHECK_EQUAL( RootB->getChecked(), Qt::Checked );
+
+
+
+}
 
