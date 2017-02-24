846cbd66827d678f9648d632c0606cee7a4c92a6
Sven Steckmann <spm_gitcommit@steckmann.de>
2017-02-24 21:27:14 +0100

    Coding style.

--- logWitch/ContextMenuManipulateHeader.cpp
+++ logWitch/ContextMenuManipulateHeader.cpp
@@ -12,72 +12,77 @@
 #include "GUITools/SlotToBoostFunction.h"
 #include "GUITools/SynchronizedHeaderView.h"
 
-ContextMenuManipulateHeader::ContextMenuManipulateHeader( QHeaderView *parent )
-: QMenu( parent )
-, m_header( parent )
+ContextMenuManipulateHeader::ContextMenuManipulateHeader(QHeaderView *parent)
+:QMenu(parent)
+, m_header(parent)
 {
-    m_hideAction = addAction ( tr("hide") );
-    m_showMenu = addMenu( tr("show") );
+  m_hideAction = addAction(tr("hide"));
+  m_showMenu = addMenu(tr("show"));
 
-    QObject::connect(m_header, SIGNAL(customContextMenuRequested(const QPoint &)),
-                     this, SLOT(contextMenuRequest(const QPoint &)));
+  QObject::connect(m_header, SIGNAL(customContextMenuRequested(const QPoint &)),
+      this, SLOT(contextMenuRequest(const QPoint &)));
 
-    QObject::connect(m_hideAction, SIGNAL( triggered() ),
-                     this, SLOT( hideColumn() ) );
+  QObject::connect(m_hideAction, SIGNAL(triggered()), this, SLOT(hideColumn()));
 }
 
-void ContextMenuManipulateHeader::contextMenuRequest( const QPoint & pos )
+void ContextMenuManipulateHeader::contextMenuRequest(const QPoint & pos)
 {
-    m_headerToWorkOn = m_header->logicalIndexAt( pos );
+  m_headerToWorkOn = m_header->logicalIndexAt(pos);
 
-    // Configure menu ....
-    m_showMenu->clear();
+  // Configure menu ....
+  m_showMenu->clear();
 
-    SynchronizedHeaderView *syncHeader = dynamic_cast<SynchronizedHeaderView *>(m_header);
+  SynchronizedHeaderView *syncHeader =
+      dynamic_cast<SynchronizedHeaderView *>(m_header);
 
-    for( int i = 0; i < m_header->model()->columnCount(); i++ )
+  for (int i = 0; i < m_header->model()->columnCount(); i++)
+  {
+    if (m_header->isSectionHidden(i))
     {
-        if( m_header->isSectionHidden( i ) )
-        {
-            QVariant v = m_header->model()->headerData( i, Qt::Horizontal, Qt::DisplayRole );
-            QString name = v.toString();
-            QAction *action = m_showMenu->addAction( name );
+      QVariant v = m_header->model()->headerData(i, Qt::Horizontal,
+          Qt::DisplayRole);
+      QString name = v.toString();
+      QAction *action = m_showMenu->addAction(name);
 
-            if( syncHeader )
-            {
-                connect( action,SIGNAL(triggered()),
-                    // Destruction of this will be handled by the action itself.
-                    new SlotToBoostFunction(action,boost::bind(&SynchronizedHeaderView::showSection,syncHeader,i)),
-                    SLOT(handleSignal()));
-            }
-            else
-            {
-                connect( action,SIGNAL(triggered()),
-                    // Destruction of this will be handled by the action itself.
-                    new SlotToBoostFunction(action,boost::bind(&QHeaderView::showSection,m_header,i)),
-                    SLOT(handleSignal()));
-            }
-        }
+      if (syncHeader)
+      {
+        connect(action, SIGNAL(triggered()),
+        // Destruction of this will be handled by the action itself.
+            new SlotToBoostFunction(action,
+                boost::bind(&SynchronizedHeaderView::showSection, syncHeader,
+                    i)), SLOT(handleSignal()));
+      }
+      else
+      {
+        connect(action, SIGNAL(triggered()),
+            // Destruction of this will be handled by the action itself.
+            new SlotToBoostFunction(action,
+                boost::bind(&QHeaderView::showSection, m_header, i)),
+            SLOT(handleSignal()));
+      }
     }
+  }
 
-    m_showMenu->setEnabled( !m_showMenu->isEmpty() );
+  m_showMenu->setEnabled(!m_showMenu->isEmpty());
 
-    m_hideAction->setEnabled( m_header->hiddenSectionCount() < m_header->count() - 1 );
+  m_hideAction->setEnabled(
+      m_header->hiddenSectionCount() < m_header->count() - 1);
 
-    popup( m_header->mapToGlobal( pos ) );
+  popup(m_header->mapToGlobal(pos));
 }
 
 void ContextMenuManipulateHeader::hideColumn()
 {
-    // Ensure at least one column is not hidden ....
-    if( m_header->hiddenSectionCount() < m_header->count() - 1 )
-    {
-        // This is necessary, because these methods are not virtual in QT *grr*
-        SynchronizedHeaderView *syncHeader = dynamic_cast<SynchronizedHeaderView *>(m_header);
-        if( syncHeader )
-            syncHeader->hideSection( m_headerToWorkOn );
-        else
-            m_header->hideSection( m_headerToWorkOn );
-    }
+  // Ensure at least one column is not hidden ....
+  if (m_header->hiddenSectionCount() < m_header->count() - 1)
+  {
+    // This is necessary, because these methods are not virtual in QT *grr*
+    SynchronizedHeaderView *syncHeader =
+        dynamic_cast<SynchronizedHeaderView *>(m_header);
+    if (syncHeader)
+      syncHeader->hideSection(m_headerToWorkOn);
+    else
+      m_header->hideSection(m_headerToWorkOn);
+  }
 }
 
